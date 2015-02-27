d98d86fc788bdde8f9d2a42a38d604451d0b7fa6
Joel Byron <jrbyron@gmail.com>
2015-02-26 20:59:43 -0500

    added edit feature!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,8 +8,33 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params.require(:event).permit(:title, :body))
+    if @event.save
+      flash[:notice] = "Event was saved."
+      redirect_to @event
+    else
+      flash[:error] = "There was an error saving the event. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
+
+def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(params.require(:event).permit(:title, :body))
+      flash[:notice] = "Event was updated."
+      redirect_to @event
+    else
+      flash[:error] = "There was an error saving the event. Please try again."
+      render :edit
+    end
+end
+
 end
