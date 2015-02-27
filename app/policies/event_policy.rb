86d61ddd93fd50987358b9324283cc7a5427f7ff
Joel Byron <jrbyron@gmail.com>
2015-02-27 01:13:53 -0500

    fixed minor if policy bugs for showing "new event" button on index

--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -2,4 +2,16 @@ class EventPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def create?
+  	user.admin?
+  end
+
+  def new?
+  	create?
+  end
+
+  def update?
+    create?
+  end
 end
