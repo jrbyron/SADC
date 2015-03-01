a9a53d0adf313f5d80b58868e6c19a6266bc3517
Joel Byron <jrbyron@gmail.com>
2015-03-01 03:25:45 -0500

    created popular posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+  end
+
   private
 
   def user_params
