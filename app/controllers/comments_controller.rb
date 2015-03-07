fe16c6a136e2387f9f94e6dc64de7275213d1fc9
Joel Byron <jrbyron@gmail.com>
2015-03-06 23:45:21 -0500

    fixed mistake from ajax commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,26 +2,32 @@ class CommentsController < ApplicationController
   
   def create
     @post = Post.find(params[:post_id])
-    @comments = @post.comments
-
-    @comment = current_user.comments.build( comment_params )
-    @comment.post = @post
-
-    authorize @comment
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:notice] = "Comment was created."
+      redirect_to [@post.topic, @post], notice: "Comment saved successfully."
     else
-      flash[:error] = "There was an error saving the comment. Please try again."
+      redirect_to [@post.topic, @post], notice: "Comment failed to save."
     end
+  end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    authorize @comment
+
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+    end
+    
     respond_to do |format|
       format.html
       format.js
     end
   end
-
-  ...
 
   private
 
